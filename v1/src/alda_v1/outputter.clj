5e1e2769d9096a0c848d08b6bfd0bfd8047747d9
Dave Yarwood <dave.yarwood@gmail.com>
2020-02-25 14:20:14 -0500

    v1 outputter

---
+++ v1/src/alda_v1/outputter.clj
@@ -0,0 +1,21 @@
+(ns alda-v1.outputter
+  (:require [alda.parser :as parser]
+            [clojure.math.numeric-tower :as math])
+  (:import [alda.lisp.model.records Note]))
+
+(defn output
+  [{:keys [events]}]
+  (println "offset,duration,midi note")
+  (doseq [event events
+          :when (or (instance? Note event)
+                    (throw (ex-info "Unexpected event type"
+                                    {:event event
+                                     :type (type event)})))
+          :let [{:keys [offset duration midi-note]} event]]
+    (println (format "%s,%s,%s"
+                     (math/round offset)
+                     (math/round duration)
+                     midi-note))))
+
+(defn -main [filename]
+  (-> filename slurp parser/parse-input output))
